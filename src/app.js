338dfb0aa6669f8eb88f562996c72f04b92517f5
Diego Aguirre <aguirrediego734@gmail.com>
2022-08-02 16:12:24 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const app = require('./server');
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log(`server on port ${PORT}`);
+})
---
+++ src/controllers/index.controller.js
@@ -0,0 +1,74 @@
+
+let usuario = [
+    {
+        'id':0,
+        'nombre':'pepe'
+    },
+    {
+        'id':1,
+        'nombre':'juan'
+    },
+    {
+        'id':2,
+        'nombre':'jony'
+    }
+]
+
+const index = async (req,res) => {
+    return res.render('../src/views/usuario/index', {usuario});
+};
+
+const show = async (req,res) => {
+    const id = req.params.id
+    const elemento = usuario[id]
+    return res.render('../src/views/usuario/show', {elemento});
+};
+
+const edit = async (req,res) => {
+    const id = req.params.id
+    const elemento = usuario[id]
+    return res.render('../src/views/usuario/edit', {elemento});
+};
+
+const create = async (req,res) => {
+    return res.render('../src/views/usuario/create');
+};
+
+//API
+
+const store = async (req,res) => {
+    const {nombre,id} = req.body;
+    if (nombre) {
+        usuario.unshift({id,nombre})
+        return res.status(200).json({'status':200, id, nombre, 'msg':'Creado correctamente'})
+    } else {
+        return res.status(404).json({'msg':'No se recibieron los datos'})
+    }
+};
+
+const update = async (req,res) => {
+    const {nombre} = req.body;
+    const id = req.params.id;
+    if (id) {
+        const actualizado = usuario.splice(id,1,{'id':id,'nombre':nombre})
+        return res.status(201).json({'status':201, actualizado, 'msg':'Editado correctamente'})
+    } else {
+        return res.status(404).json({'msg':'No se recibieron los datos'})
+    }
+};
+
+const destroy = async (req,res) => {
+    const id = req.params.id;
+    const eliminado = usuario.splice(id,1)
+    return res.status(200).json({'status':200,eliminado, 'msg':'Eliminado correctamente'})
+};
+
+module.exports = {
+    index,
+    create,
+    show,
+    edit,
+    store,
+    destroy,
+    update
+};
